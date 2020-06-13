5bbcd9bc38f1f51b90fc6fb7e35af22a76fb20be
Nathan <nathan@bluemesaengineering.com>
2020-06-13 18:42:07 -0400

    feat:Set up overlap map component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router } from '@reach/router'
 import './App.css';
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Home from './pages/Home.jsx'
+import Blog from './pages/Blog.jsx'
+import ResponsiveNavigation from './Components/ResponsiveNavigation'
+import logo from './logo.svg'
 
 function App() {
+
+  const navLinks = [
+    {
+      text: 'Home',
+      path: '/',
+      icon: 'ion-ios-home',
+    },
+    {
+      text: 'Contact',
+      path: '/contact',
+      icon: 'ion-ios-megaphone',
+    },
+    {
+      text: 'About',
+      path: '/about',
+      icon: 'ion-ios-business',
+    },
+    {
+      text: 'Blog',
+      path: '/blog',
+      icon: 'ion-ios-bonfire',
+    },
+    {
+      text: 'Portfolio',
+      path: '/portfolio',
+      icon: 'ion-ios-briefcase',
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveNavigation 
+        navLinks={ navLinks }
+        logo = { logo }
+      />
+      <Router>
+        < Contact path = "/contact" />
+        < Portfolio path = "/portfolio" />
+        < Home path = "/home" />
+        < Blog path = "/blog" />
+        < About path = "/About" />
+      </Router>
     </div>
   );
 }
